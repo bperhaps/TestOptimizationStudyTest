cfb59ae807050258e5694416beb89606de628706
Minsung Son <sns2831@gmail.com>
2021-09-29 16:38:22 +0900

    feat: acceptanceTest without DirtiesContext

--- src/test/java/com/example/testoptimizationstudytest/AcceptanceTest.java
+++ src/test/java/com/example/testoptimizationstudytest/AcceptanceTest.java
@@ -1,22 +1,34 @@
 package com.example.testoptimizationstudytest;
 
+import com.example.testoptimizationstudytest.config.DatabaseCleaner;
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.annotation.DirtiesContext.ClassMode;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 public class AcceptanceTest {
 
     @LocalServerPort
     private int port;
 
+    @Autowired
+    private DatabaseCleaner databaseCleaner;
+
     @BeforeEach
     void setUp() {
         RestAssured.port = port;
+    }
+
+    @AfterEach
+    void tearDown() {
+        clearDataBase();
+    }
+
+    private void clearDataBase() {
+        databaseCleaner.execute();
     }
 }
