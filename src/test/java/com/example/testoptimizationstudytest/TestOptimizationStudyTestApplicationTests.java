7754775c0b75151cc9a1f7516537a8e127e22df8
Minsung Son <sns2831@gmail.com>
2021-09-29 15:36:18 +0900

    Initial commit

---
+++ src/test/java/com/example/testoptimizationstudytest/TestOptimizationStudyTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testoptimizationstudytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestOptimizationStudyTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
